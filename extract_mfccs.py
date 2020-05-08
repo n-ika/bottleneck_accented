c96fb510bfdb2183eb57f2fa0c6f442b25b0ca39
n-ika <nika.jurov@gmail.com>
2020-05-08 12:55:41 +0200

    add mfcc script

---
+++ extract_mfccs.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import pqkmeans
+import pickle
+import sys
+import os
+from python_speech_features import mfcc
+import scipy.io.wavfile as wav
+import glob
+
+folder="/Users/nika/Desktop/corpus_accent"
+
+features = {}
+for filepath in glob.iglob(folder + '/*/*.wav', recursive=True):
+    wav_file = filepath.split('/')[-1]
+    (rate,sig) = wav.read(filepath)
+    mfcc_feat = mfcc(sig,rate)
+    features[wav_file] = [mfcc_feat]
+
+df = pd.DataFrame.from_dict(features, orient='index')
+
+d = dict()
+for key in features:
+    # access every features object
+    feats = features[key]
+    d[key] = [feats]
+
+df.to_csv('./out_feats.csv')
